89e1efd49f622b095f68fc94a84828b584b43833
SAKIBUL ISLAM KHAN <sakibul@live.com>
2020-05-15 20:09:30 -0400

    Create the initial solution for a Registration form and SQL Server.

---
+++ RegistrationSQLServer/RegistrationSQLServer/DatabaseLayer/DBUtlities.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace RegistrationSQLServer.DatabaseLayer
+{
+    public class DBUtlities // ADO.NET code
+    {
+        
+
+        private static SqlConnection GetSqlConnection() // this method is private as it will never be called from outside
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ToString();
+
+            return new SqlConnection(connectionString);
+        }
+
+        public static int InsertUserInformation(BusinessLayer.UserInformation userInfo)
+        {
+            // They are not handled by CLR Garbage Collector, so using three using
+            int result = -1;
+            String sql = "";
+
+            using (SqlConnection cnn = GetSqlConnection())
+            {
+                sql = "Insert into UserInformation(FirstName, Lastname, Address, City, Province, PostalCode, Country) " +
+                        $"values ('{userInfo.FirstName}', '{userInfo.LastName}', '{userInfo.Address}', '{userInfo.City}', '{userInfo.Province}', '{userInfo.PostalCode}', '{userInfo.Country}')";
+
+                using (SqlCommand command = new SqlCommand(sql, cnn))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter())
+                    {
+                        cnn.Open();
+                        adapter.InsertCommand = new SqlCommand(sql, cnn);
+                        result = adapter.InsertCommand.ExecuteNonQuery(); // 1 == OK
+                    }
+                } // command.Dispose();
+            }
+            return result;
+        }
+    }
+}
---
+++ RegistrationSQLServer/RegistrationSQLServer/Registration.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RegistrationSQLServer
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void enterInfoButton_Click(object sender, EventArgs e)
+        {
+            if(Page.IsValid)
+            {
+                // send the user information to the database:
+
+                BusinessLayer.UserInformation userInfo = new BusinessLayer.UserInformation();
+
+                userInfo.FirstName = Server.HtmlEncode(firstNameTextBox.Text);
+                userInfo.LastName = Server.HtmlEncode(lastNameTextBox.Text);
+                userInfo.Address = Server.HtmlEncode(addressTextBox.Text);
+                userInfo.City = Server.HtmlEncode(cityTextBox.Text);
+                userInfo.Province = Server.HtmlEncode(stateOrProvinceTextBox.Text);
+                userInfo.PostalCode = Server.HtmlEncode(zipCodeTextBox.Text);
+                userInfo.Country = Server.HtmlEncode(countryTextBox.Text);
+
+                // DatabaseLayer.DBUtlities.InsertUserInformation(userInfo);
+
+                if(DatabaseLayer.DBUtlities.InsertUserInformation(userInfo) == 1)
+                    this.lblResultMessage.Text = "The user information was successfully inserted into DB table.";
+                else
+                    this.lblResultMessage.Text = "There was an error while inserting user information !!!";
+
+                /*
+                Session["firstName"] = Server.HtmlEncode(firstNameTextBox.Text);
+                Session["lastName"] = Server.HtmlEncode(lastNameTextBox.Text);
+                Session["address"] = Server.HtmlEncode(addressTextBox.Text);
+                Session["city"] = Server.HtmlEncode(cityTextBox.Text);
+                Session["stateOrProvince"] = Server.HtmlEncode(stateOrProvinceTextBox.Text);
+                Session["zipCode"] = Server.HtmlEncode(zipCodeTextBox.Text);
+                Session["country"] = Server.HtmlEncode(countryTextBox.Text);
+                */
+            }
+        }
+    }
+}
